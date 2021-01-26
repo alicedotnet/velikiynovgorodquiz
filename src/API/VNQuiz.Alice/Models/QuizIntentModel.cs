ca3bde01191f6d63ab35f861219220c0cd256512
alexvolchetsky <alex.volchetsky@gmail.com>
2021-01-26 22:28:57 +0300

    move logic to library

--- src/API/VNQuiz.Alice/Models/QuizIntentModel.cs
+++ src/API/VNQuiz.Alice/Models/QuizIntentModel.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Yandex.Alice.Sdk;
 using Yandex.Alice.Sdk.Models;
 
 namespace VNQuiz.Alice.Models
 {
     public class QuizIntentModel
     {
-        [JsonPropertyName("YANDEX.CONFIRM")] //TODO::move this to library
-        public AliceIntentModel<object> YandexConfirm { get; set; } //TODO::create non-generic version of AliceIntentModel
+        [JsonPropertyName(AliceConstants.AliceIntents.Confirm)]
+        public AliceIntentModel YandexConfirm { get; set; }
 
         [JsonPropertyName("confirm")]
-        public AliceIntentModel<object> Confirm { get; set; } //TODO::create non-generic version of AliceIntentModel
+        public AliceIntentModel Confirm { get; set; }
 
         public bool IsConfirm
         {
@@ -23,11 +24,11 @@ namespace VNQuiz.Alice.Models
             }
         }
 
-        [JsonPropertyName("YANDEX.REJECT")] //TODO::move this to library
-        public AliceIntentModel<object> YandexReject { get; set; } //TODO::create non-generic version of AliceIntentModel
+        [JsonPropertyName(AliceConstants.AliceIntents.Reject)]
+        public AliceIntentModel YandexReject { get; set; }
 
         [JsonPropertyName("reject")]
-        public AliceIntentModel<object> Reject { get; set; } //TODO::create non-generic version of AliceIntentModel
+        public AliceIntentModel Reject { get; set; }
     
         public bool IsReject
         {
